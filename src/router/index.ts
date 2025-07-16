a788039a51f791cfb5a2a7c7eb0f5803c927824b
RobertPintera <robert.it.990@gmail.com>
2025-07-16 23:24:18 +0200

    Configure routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,11 @@
 import RecordsView from "@/pages/RecordsView.vue";
 import StatisticsView from "@/pages/StatisticsView.vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
 
-const routes = [
-  { path: '/records', name: 'Records', component: RecordsView },
-  { path: '/statistics', name: 'About', component: StatisticsView }
+const routes: RouteRecordRaw[]  = [
+  { path: '/', redirect: '/records' },
+  { path: '/records', component: RecordsView },
+  { path: '/statistics', component: StatisticsView }
 ];
 
 export default createRouter({
