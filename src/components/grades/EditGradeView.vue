c15602788166665c926c625c36722a2114ef9ee1
RobertPintera <robert.it.990@gmail.com>
2025-08-27 01:58:20 +0200

    Install VeeValidate (problems with inputNumber from PrimeVue)

--- src/components/grades/EditGradeView.vue
+++ src/components/grades/EditGradeView.vue
@@ -3,26 +3,33 @@ import type { Grade } from '@/models/Grade';
 import type { Test } from '@/models/Test';
 import { useGradesStore } from '@/stores/useGradesStore';
 import { useTestsStore } from '@/stores/useTestsStore';
-import type { Form, FormSubmitEvent } from '@primevue/forms';
-import { zodResolver } from '@primevue/forms/resolvers/zod';
-import { computed, reactive, ref, watch } from 'vue';
-import z from 'zod';
+import { reactive, ref, watch } from 'vue';
+import * as yup from 'yup';
+import { useForm } from 'vee-validate';
 
 const testsStore = useTestsStore();
 const gradesStore = useGradesStore();
 
 const visible = defineModel<boolean>('visible', {default: false});
-
 const props = defineProps<{ studentId: string, testId: string }>();
-
-const formRef = ref<InstanceType<typeof Form> | null>(null);
-
 
 const test = reactive<Pick<Test, 'name' | 'maxPoints'>>({
   name: "",
   maxPoints: 0
 });
-const score = ref<number | null>(null);
+
+const schema = yup.object({
+  score: yup
+    .number()
+    .nullable()
+    .min(0, 'Score cannot be less than 0')
+});
+
+const { defineField, handleSubmit, resetForm, errors } = useForm({
+  validationSchema: schema,
+});
+
+const [score] = defineField('score');
 
 const updateDataForm = (visible: boolean) =>{
   if (visible) {
@@ -44,19 +51,9 @@ const cancel = () => {
   visible.value = false;
 };  
 
-const resolver = computed(() => zodResolver(
-  z.object({
-    score: z.number("Score is required")
-      .min(0,"Score cannot be less than 0")
-      .max(test.maxPoints, `Score cannot be greater than ${test.maxPoints}`)
-      .nullable()
-  })
-));
-
-const submit = (event: FormSubmitEvent<Record<string, any>>) => {
-  console.log(event)
-  if (event.valid) {
-    const score = event.states.score?.value;
+const onSubmit = handleSubmit((values) => {
+  if (values) {
+    const score = values.score;
 
     if(typeof score === 'number' || score === null){
       const grade: Grade = {
@@ -77,12 +74,12 @@ const submit = (event: FormSubmitEvent<Record<string, any>>) => {
       console.error("Wrong data from form!");
     }
   }
-};
+});
 </script>
 
 <template>
   <Dialog header="Edit Grade" v-model:visible="visible" :modal="true" :draggable="false" class="w-60">
-    <Form v-slot="$form" :resolver="resolver" @submit="submit" 
+    <form @submit="onSubmit" 
     class="flex flex-col gap-3 mt-2 w-full">
       <h5 class="text-center">Number must be between 0 and {{ test.maxPoints }}</h5>
       <div class="flex flex-col gap-1">
@@ -91,12 +88,12 @@ const submit = (event: FormSubmitEvent<Record<string, any>>) => {
           v-model="score" :allowEmpty="true" :class="'w-full'"/>
           <label for="score">Score</label>
         </FloatLabel>
-        <Message v-if="$form.score?.invalid" severity="error" size="small" variant="simple">{{ $form.score.error?.message }}</Message>
+        <Message v-if="errors.score" severity="error" size="small" variant="simple">{{ errors.score }}</Message>
       </div>
       <div class="flex justify-end gap-2 mt-4">
         <Button label="Cancel" variant="outlined" icon="pi pi-times" @click="cancel" />
         <Button label="Save" icon="pi pi-check" type="submit" autofocus />
       </div>
-    </Form>
+    </form>
   </Dialog>
 </template>
