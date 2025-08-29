a744c447d7d35d6fe8af0709aa99e339c71e6478
RobertPintera <robert.it.990@gmail.com>
2025-08-29 21:12:34 +0200

    Separate cards of settings to files and add dialog for delete test

---
+++ src/components/settings/cards/Export.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Card from '@/components/core/Card.vue';
+
+
+</script>
+
+<template>
+  <Card>
+    <template #header>
+      <h3>Export</h3>
+    </template>
+    <template #body>
+      <div class="flex flex-col gap-4">
+        <div class="flex flex-col gap-1">
+          <h4>Save data (in progress)</h4>
+          <Button label="Save" class="w-fit"/>
+        </div>
+      </div>
+    </template>
+  </Card>
+</template>
---
+++ src/components/settings/cards/Import.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import Card from '@/components/core/Card.vue';
+import { useRegisterStore } from '@/stores/useRegisterStore';
+import { useConfirm, useToast } from 'primevue';
+
+const registerStore = useRegisterStore();
+const confirm = useConfirm();
+const toast = useToast();
+
+const loadDemoDataDialog = () => {
+  confirm.require({
+    message: 'Are you sure you want to proceed? Previous data will be deleted.',
+    header: 'Load demo data',
+    icon: 'pi pi-exclamation-triangle',
+    rejectProps: {
+      label: 'Cancel',
+      severity: 'secondary',
+      outlined: true
+    },
+    acceptProps: {
+      label: 'Load'
+    },
+    accept: async () => {
+      try {
+        await registerStore.loadDemoData();
+        toast.add({ 
+          severity: 'success', 
+          summary: 'Success', 
+          detail: 'Demo data was loaded', 
+          life: 3000 
+        });
+      } catch (err) {
+        toast.add({ 
+          severity: 'danger', 
+          summary: 'Error', 
+          detail: 'Failed to load demo data', 
+          life: 3000 
+        });
+      }
+    },
+  });
+};  
+</script>
+
+<template>
+  <Card>
+    <template #header>
+      <h3>Import</h3>
+    </template>
+    <template #body>
+      <div class="flex flex-col gap-4">
+        <div class="flex flex-col gap-1">
+          <h4>Load demo data</h4>
+          <Button label="Load" class="w-fit" @click="loadDemoDataDialog()"/>
+        </div>
+        <div class="flex flex-col gap-1">
+          <h4>Load data from device (in progress)</h4>
+          <Button label="Load" class="w-fit"/>
+        </div>
+      </div>
+    </template>
+  </Card>
+</template>
