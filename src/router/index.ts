fd3e80574047f11abcbe47e363bc86e7325a7893
RobertPintera <robert.it.990@gmail.com>
2025-08-31 14:23:36 +0200

    Change to WebHashHistory in index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import GradesView from "@/views/GradesView.vue";
 import StudentsListView from "@/views/StudentsListView.vue";
 import StudentDetailsView from "@/views/StudentDetailsView.vue";
 
-import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, createWebHistory, type RouteRecordRaw } from "vue-router";
 import TestsListView from "@/views/TestsListView.vue";
 import SettingsView from "@/views/SettingsView.vue";
 import TestDetailsView from "@/views/TestDetailsView.vue";
@@ -25,7 +25,7 @@ const routes: RouteRecordRaw[]  = [
 ];
 
 export default createRouter({
-  history: createWebHistory(
+  history: createWebHashHistory(
     import.meta.env.PROD ? '/Class-register/' : '/'
   ),
   routes
