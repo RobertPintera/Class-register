233af5704ca4ae10d788962e0b5761200a7e00c0
RobertPintera <robert.it.990@gmail.com>
2025-09-23 23:49:57 +0200

    Refactor carousel logic for infinite scrolling

--- src/components/core/Footer.vue
+++ src/components/core/Footer.vue
@@ -4,6 +4,6 @@
 
 <template>
   <footer class="p-4 w-full bg-footer-background justify-center text-center">
-    <p>Project hosted on <a href="https://github.com/RobertPintera/Class-register" class="text-gray-800 hover:text-blue-600 underline">Github</a></p>
+    <p>Project hosted on <a href="https://github.com/RobertPintera/Class-register" class="hover:text-blue-600 underline">Github</a></p>
   </footer>
 </template>
--- src/components/home/presentationSection/Carousel.vue
+++ src/components/home/presentationSection/Carousel.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import { computed, nextTick, onBeforeMount, onBeforeUnmount, onMounted, ref } from 'vue';
 
+interface itemTemplate {
+  ref: HTMLElement;
+  width: number;
+  startPosition: number;
+};
+
 const images = [
   {
     alt: "Dashboard",
@@ -32,55 +38,166 @@ const images = [
   },
 ];
 
-const container = ref<HTMLElement | null>(null);
-const itemRefs = ref<HTMLElement[]>([]);
-const currentIndex = ref<number>(Math.floor(images.length/2));
+const container = ref<HTMLElement | null>();
+const itemActive = ref<number>(0);
+const items = ref<itemTemplate[]>([]);
+const totalWidth = ref<number>(0);
 const offset = ref<number>(0);
-const isResizing = ref(false);
-let resizeTimeout: number | undefined;
+const gap = 32;
 
 const next = () => {
-  currentIndex.value = (currentIndex.value + 1) % images.length;
-  recenter();
+  itemActive.value = (itemActive.value + 1) % items.value.length;
+  centerToItemAt(itemActive.value);
+  updatePositionItems();
 };
 
 const previous = () => {
-  currentIndex.value = (currentIndex.value - 1 + images.length) % images.length;
-  recenter();
+  itemActive.value = (itemActive.value - 1 + items.value.length) % items.value.length;
+  centerToItemAt(itemActive.value);
+  updatePositionItems();
 };
 
+const initData = () => {
+  items.value.forEach(item => {
+    item.width = item.ref.offsetWidth;
+    item.startPosition =item.ref.offsetLeft;
+  });
+
+  const widths = items.value.map(el => el.width);
+  const gapsTotal = (images.length - 1) * gap;
+  totalWidth.value = widths.reduce((sum, w) => sum + w, 0) + gapsTotal;
+  
+  console.log(totalWidth.value);
+};
+
+function animateOffset(target: number, duration = 400) {
+  const start = performance.now();
+  const initial = offset.value;
+
+  function step(now: number) {
+    const progress = Math.min((now - start) / duration, 1);
+    const eased = 1 - Math.pow(1 - progress, 3);
+    offset.value = (initial + (target - initial) * eased);
+    offset.value = ((offset.value % totalWidth.value) + totalWidth.value) % totalWidth.value;
+    updatePositionItems();
+
+    if (progress < 1) requestAnimationFrame(step);
+  }
+
+  requestAnimationFrame(step);
+}
+
+const updatePositionItems = () => {
+  if (!container.value) return;
+  
+  items.value.forEach((item, index) => {
+    const futurePosition = item.startPosition + offset.value + item.width;
+    
+    if(futurePosition > totalWidth.value) {
+      item.ref.style.transform = `translateX(${offset.value - totalWidth.value - gap}px)`;
+    }
+    else if(futurePosition < 0){ 
+      item.ref.style.transform = `translateX(${totalWidth.value - offset.value  + gap}px)`; 
+    }
+    else
+    {
+      item.ref.style.transform = `translateX(${offset.value}px)`;
+    }
+
+  });
+};
+
+const centerToItem = () => {
+  if (!container.value || items.value.length === 0) return;
+
+  const containerRect = container.value.getBoundingClientRect();
+  const containerCenter = containerRect.width / 2;
+
+  let closestIndex = 0;
+  let minDistance = Infinity;
+
+  items.value.forEach((item, index) => {
+    const rect = item.ref.getBoundingClientRect();
+    const itemCenter = (rect.left - containerRect.left) + rect.width / 2;
+    const distance = Math.abs(itemCenter - containerCenter);
+
+    if (distance < minDistance) {
+      minDistance = distance;
+      closestIndex = index;
+    }
+  });
+
+  const targetRect = items.value[closestIndex].ref.getBoundingClientRect();
+  const targetCenter = (targetRect.left - containerRect.left) + targetRect.width / 2;
+  const offsetToCenter = containerCenter - targetCenter;
+
+  animateOffset(offset.value + offsetToCenter);
+  itemActive.value = closestIndex;
+
+  return { offsetToCenter: offsetToCenter, closestIndex: closestIndex };
+};
+
+const centerToItemAt = (index: number) => {
+  if (!container.value) return;
+  const containerRect = container.value.getBoundingClientRect();
+  const containerCenter = containerRect.width / 2;
+
+  const targetRect = items.value[index].ref.getBoundingClientRect();
+  const targetCenter = (targetRect.left - containerRect.left) + targetRect.width / 2;
+  const offsetToCenter = containerCenter - targetCenter;
+
+  animateOffset(offset.value + offsetToCenter);
+  itemActive.value = index;
+
+  return { offsetToCenter: offsetToCenter};
+};
+
+let isDragging = false;
+let startX = 0;
+let startOffset = 0;
+
+const onDragStart = (e: MouseEvent | TouchEvent) => {
+  isDragging = true;
+  startX = 'touches' in e ? e.touches[0].clientX : e.clientX;
+  startOffset = offset.value;
+  updatePositionItems();
+};
+
+
+const onDragMove = (e: MouseEvent | TouchEvent) => {
+  if (!isDragging) return;
+  const currentX = 'touches' in e ? e.touches[0].clientX : e.clientX;
+  const delta = currentX - startX;
+  offset.value = startOffset + delta;
+  updatePositionItems();
+};
+
+const onDragEnd = () => {
+  if (!isDragging) return;
+  isDragging = false;
+  centerToItem();
+  updatePositionItems();
+};
 
 const handleResize = () => {
-  isResizing.value = true;
-
-  recenter();
-
-  clearTimeout(resizeTimeout);
-  resizeTimeout = window.setTimeout(() => {
-    isResizing.value = false;
-  }, 150);
-};
-
-const recenter = () => {
-  if (!container.value || !itemRefs.value[currentIndex.value]) return;
-
-  const contRect = container.value.getBoundingClientRect();
-  const activeRect = itemRefs.value[currentIndex.value].getBoundingClientRect();
-
-  const contCenter = contRect.left + contRect.width / 2;
-  const activeCenter = activeRect.left + activeRect.width / 2;
-
-  offset.value += contCenter - activeCenter;
+  initData();
+  const result = centerToItem();
+  offset.value = result.offsetToCenter;
+  itemActive.value = result?.closestIndex;
+  updatePositionItems();
 };
 
 onMounted(() => {
   // Wait a tick to ensure the DOM is fully rendered before calculating positions
-  setTimeout(() => recenter(), 0);
-  window.addEventListener("resize", handleResize);
+  setTimeout(() => {
+    handleResize();
+  }, 0);
+
+  window.addEventListener('resize', handleResize);
 });
 
 onBeforeUnmount(() => {
-  window.removeEventListener("resize", handleResize);
+  window.removeEventListener('resize', handleResize);
 });
 
 </script>
@@ -90,14 +207,20 @@ onBeforeUnmount(() => {
     <Button icon="pi pi-arrow-left" rounded variant="outlined" @click="previous" class="absolute top-1/2 left-8 z-20"/>
     <Button icon="pi pi-arrow-right" rounded variant="outlined" @click="next" class="absolute top-1/2 right-8 z-20"/>
 
-    <div class="absolute left-0 top-0 h-full w-64 bg-gradient-to-r from-primary-background to-transparent z-10"/> 
-    <div class="absolute right-0 top-0 h-full w-64 bg-gradient-to-l from-primary-background to-transparent z-10"/>
+    <!-- <div class="absolute left-0 top-0 h-full w-64 bg-gradient-to-r from-primary-background to-transparent z-10 pointer-events-none"/> 
+    <div class="absolute right-0 top-0 h-full w-64 bg-gradient-to-l from-primary-background to-transparent z-10 pointer-events-none"/> -->
 
-    <div ref="container" class="relative overflow-hidden">
-      <ul class="flex gap-8 transition-transform" 
-      :class="{ 'transition-none': isResizing }" 
-      :style="{ transform: `translateX(${offset}px)` }">
-        <li v-for="(img, i) in images" :key="i"  :ref="el => { if (el) itemRefs[i] = el as HTMLElement }" class="flex-shrink-0 ">
+    <div ref="container" class="relative overflow-auto"
+      @mousedown="onDragStart"
+      @mousemove="onDragMove"
+      @mouseup="onDragEnd"
+      @mouseleave="onDragEnd"
+      @touchstart="onDragStart"
+      @touchmove="onDragMove"
+      @touchend="onDragEnd">
+      <ul class="flex gap-8">
+        <!--  -->
+        <li v-for="(img, i) in images" :key="i"  :ref="el => { if (el) items[i] = { ref: el as HTMLElement, width: 0, startPosition: 0 } }" class="flex-shrink-0">
           <img :src="img.src" :alt="img.alt"
             class="w-full min-w-[128px] max-w-[50vw] border-4 border-primary-border rounded-2xl select-none"
             :draggable="false"
